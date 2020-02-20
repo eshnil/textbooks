706a995cd2666ffb3d8d9172d24c2d19751ea6f0
odyseeus <hdtycho@gmail.com>
2020-02-21 04:58:35 +0530

    Complex numbers are Drag-able

--- content/complex/components/complextransform.ts
+++ content/complex/components/complextransform.ts
@@ -5,28 +5,21 @@
 
 import {flatten, tabulate2D} from '@mathigon/core';
 import {Point, Line} from '@mathigon/fermat';
-import {CanvasView, CustomElementView, register, slide} from '@mathigon/boost';
+import {CanvasView, CustomElementView, register, slide,hover} from '@mathigon/boost';
 import {Complex} from '@mathigon/fermat';
 import {Select} from '../../shared/types';
+import {numberline} from "../../exploding-dots/functions";
 
 // -------------------------------------------------------------------------
 // Component
 
-let width  = 2000;
-let height = 2000;
+const width  = 2000;
+const height = 2000;
 
-let xStart = -(width/2);
-let xEnd   = (width/2);
-let yStart = -(height/2);
-let yEnd   = (height/2);
-
-let complexNumberOne  = new Complex(getRandomCoordinates(xStart,xEnd), getRandomCoordinates(yStart,yEnd)); //gets random coordinates between within the plane
-let complexNumberTwo  = new Complex(getRandomCoordinates(xStart,xEnd), getRandomCoordinates(yStart,yEnd));
-let complexSum        = Complex.sum(complexNumberOne,complexNumberTwo);
-let complexDifference = Complex.difference(complexNumberOne,complexNumberTwo);
-let complexProduct    = Complex.product(complexNumberOne,complexNumberTwo);
-let complexQuotient   = Complex.quotient(complexNumberOne,complexNumberTwo);
-
+const xStart = -(width/2);
+const xEnd   = (width/2);
+const yStart = -(height/2);
+const yEnd   = (height/2);
 
 @register('x-complex-transform', {templateId: '#complex-transform'})
 export class ComplexTransform extends CustomElementView {
@@ -34,71 +27,127 @@ export class ComplexTransform extends CustomElementView {
   ready() {
     const $canvas = this.$('canvas') as CanvasView;
     const context = $canvas.ctx;
+    context.translate((width/2), (height/2));// change the origin of the canvas to its center from the top left corner
 
-    context.translate((width/2), (height/2));
+    let complexNumberOne  = new Complex(getRandomCoordinates(xStart/2,xEnd/2), getRandomCoordinates(yStart/2,yEnd/2)); //gets random coordinates within the plane
+    let complexNumberTwo  = new Complex(getRandomCoordinates(xStart/2,xEnd/2), getRandomCoordinates(yStart/2,yEnd/2));
+    //Flags to mark the complex number to be moved
+    let complexOneActive = false;
+    let complexTwoActive = false;
+    const arithmeticOperations = ["Sum","Product","Difference","Quotient"]; // Possible arithmetic operations
+    let activeArithmeticOperation = arithmeticOperations[0]; // 0 - Sum, 1 - Difference, 2 - Product, 3 - Quotient
 
-    drawPlane(context,width,height);
+    drawPlane(context,width,height,complexNumberOne,complexNumberTwo,activeArithmeticOperation);//Initial Plane with the randomly generated complex numbers
+    slide($canvas, {
+        start: p => {//Identify which circle has been selected and flag them as active
+           let selectedComplexNumber = new Complex(Math.round((p.x-1000) /100),Math.round((p.y-1000) /100));
+          if(selectedComplexNumber.re===complexNumberOne.re && selectedComplexNumber.im===complexNumberOne.im) {
+            complexOneActive= true;
+          }else if(selectedComplexNumber.re===complexNumberTwo.re && selectedComplexNumber.im===complexNumberTwo.im) {
+            complexTwoActive= true;
+          }else{
+            console.log("Please move an existing complex number");
+          }
+        },
+        "move": p =>{// Move the active Complex number
+          let selectedComplexNumber = new Complex(Math.round((p.x-1000) /100),Math.round((p.y-1000) /100));
+           if(complexOneActive) {
+             complexNumberOne = selectedComplexNumber;
+             drawPlane(context, width, height, complexNumberOne, complexNumberTwo, activeArithmeticOperation);
+           }else if(complexTwoActive) {
+             complexNumberTwo = selectedComplexNumber;
+             drawPlane(context, width, height,complexNumberOne, complexNumberTwo, activeArithmeticOperation);
+           }
+        },
+         end: () => {// Deactivate Complex number once moved
+          complexTwoActive= false;
+         complexOneActive= false;
+         },
+      "justInside": true //Limit movement to within the canvas
+       });
 
-    drawCircle(context, complexNumberOne.re ,complexNumberOne.im);
-    drawCircle(context, complexNumberTwo.re ,complexNumberTwo.im);
-    drawCircle(context, complexSum.re,complexSum.im,"blue");
-    // drawCircle(context, complexQuotient.re,complexQuotient.im,"red");
-    // drawCircle(context, complexProduct.re,complexProduct.im,"green");
-    // drawCircle(context, complexDifference.re,complexDifference.im,"teal");
+    //Tab Switching - Changes the arithmetic operation
+    const $groups = this.$('x-select.tabs') as Select;
+    $groups.on('change', $active => {
+      clearCanvas(context);
+      activeArithmeticOperation = $active.data.value;
+      drawPlane(context,width,height,complexNumberOne,complexNumberTwo,activeArithmeticOperation);
+    });
 
-
-    // slide($canvas, {
-    //  // start: p => drawCircle(context, randomCoordinate() ,randomCoordinate()),
-    //   end: () => this.trigger('draw'),
-    //   justInside: true
-    // });
   }
 }
 
 // -------------------------------------------------------------------------
-// Utility Functions
+// Canvas Drawing Functions
 
-function drawPlane(context:CanvasRenderingContext2D, width: number, height: number) {
-  context.save();
-  context.fillStyle = 'black'; // text color
-  context.font = '48px Monospace';
+function drawPlane(context:CanvasRenderingContext2D, width: number, height:number,z1: any, z2:any, operation :string ) {
+  clearCanvas(context);
+  drawGraph(context,width,height);
 
-  // draw vertical from X to Height
+  let complexArithmetic:any;
+  let circleColor: any;
+  if(operation == "Sum"){
+    complexArithmetic = Complex.sum(z1,z2);
+    circleColor = "blue";
+  }else if(operation == "Quotient"){
+    complexArithmetic   = Complex.quotient(z1,z2);
+    circleColor = "red";
+  }else if(operation == "Product"){
+    complexArithmetic  = Complex.product(z1,z2);
+    circleColor = "green";
+  }else if(operation == "Difference"){
+    complexArithmetic = Complex.difference(z1,z2);
+    circleColor = "teal"
+  }else{
+    console.log("Invalid Argument : Complex Arithemtic Operations");
+  }
+  drawLine(context,0,0,(z1.re*100),(z1.im*100),5,"black"); // Line from 0 -> z1
+  drawLine(context,0,0,(z2.re*100),(z2.im*100),5,"black"); // Line from 0 -> z2
+  drawLine(context,(z1.re*100),(z1.im*100),(Math.round(complexArithmetic.re)*100),(Math.round(complexArithmetic.im)*100),5,circleColor); // Line from z1 -> output of Calculation
+  drawLine(context,(z2.re*100),(z2.im*100),(Math.round(complexArithmetic.re)*100),(Math.round(complexArithmetic.im)*100),5,circleColor); // Line from z2 -> output of Calculation
+
+  //Drawing Circles over the lines
+  drawCircle(context, z1.re ,z1.im);
+  drawCircle(context, z2.re ,z2.im);
+  drawCircle(context, complexArithmetic.re,complexArithmetic.im,circleColor);
+}
+
+function drawGraph(context:CanvasRenderingContext2D, width: number, height: number) {
+  context.fillStyle = 'black';
+  context.font = '45px Monospace';
+
+  // Vertical lines
   for (let x = -width; x < width; x += 100) {
     if(x===0){
-      context.lineWidth = 10;
-      context.strokeStyle = 'black';
+      drawLine(context,x,-height,x,height,10,"black");
     }
-
-    context.beginPath();
-    context.moveTo(x, -height);
-    context.lineTo(x, height);
-    context.stroke();
-    context.strokeStyle = 'gray';
-    context.lineWidth = 2;
-
-    // draw text
-    context.fillText(String(x/100), x+5, 50)
+    drawLine(context,x,-height,x,height,2);
+    context.fillText(String(x/100), x+5, 50)// Labels for the graph
   }
 
-  // draw horizontal from Y to Width
+  // Horizontal lines
   for (let y = -height; y < height; y += 100) {
     if(y===0){
-      context.lineWidth = 10;
-      context.strokeStyle = 'black';
-      context.fillStyle = 'white';
+      drawLine(context,-width,y,width,y,10,"black");
+      context.fillStyle = "rgba(255, 255, 255, 0)";
     }
-    context.beginPath();
-    context.moveTo(-width, y);
-    context.lineTo(width, y);
-    context.stroke();
-    context.strokeStyle = 'gray';
-    context.lineWidth = 2;
-
-    // draw text
-    context.fillText(String(-y/100), 10, y);
-    context.fillStyle = 'black';
+    drawLine(context,-width,y,width,y,2);
+    context.fillText(String(-y/100), 10, y);// Labels for the graph
+    context.fillStyle = "black";
   }
+}
+
+function drawLine(context:CanvasRenderingContext2D, lineStartX: number, lineStartY:number, lineEndX: number, lineEndY: number, lineThickness: number, lineColour= "grey" ) {
+  if(lineColour){
+    context.strokeStyle = lineColour;
+  }
+  if(width){
+    context.lineWidth = lineThickness;
+  }
+  context.beginPath();
+  context.moveTo(lineStartX, lineStartY);
+  context.lineTo(lineEndX, lineEndY);
+  context.stroke();
 }
 
 function drawCircle(context:CanvasRenderingContext2D  ,re: number,im: number,color: string = "black",size: number = 30) {
@@ -113,7 +162,7 @@ function drawCircle(context:CanvasRenderingContext2D  ,re: number,im: number,col
   drawLabel(context,Math.round(re),Math.round(im),coordinates);
 }
 
-function drawLabel(context:CanvasRenderingContext2D, x:number,y:number,coords) {
+function drawLabel(context:CanvasRenderingContext2D, x:number,y:number,coords:any) {
   context.fillStyle = 'black';
   if(coords.x >=xStart && coords.x <= xStart/2 || coords.x >0 && coords.x <= xEnd/2 ){//labels are placed to the right of the circle
     if((-y)<0) {
@@ -133,5 +182,14 @@ function drawLabel(context:CanvasRenderingContext2D, x:number,y:number,coords) {
 
 function getRandomCoordinates(min: number, max: number) {
   return Math.round((Math.random() * (max - min) + min)/100);
+}
+
+function clearCanvas(context: CanvasRenderingContext2D) {
+  context.save();
+
+  context.setTransform(1, 0, 0, 1, 0, 0);
+  context.clearRect(0, 0, width, height);
+
+  context.restore();
 }
 // -------------------------------------------------------------------------
